[EXTREME ESTIMATOR][MARGIN MODEL] add stationary/non-stationary margin model. fix margin formula (in the temporal case when we want a dependency on a single parameter for instance). add margin function with starting point

extreme_estimator/extreme_models/margin_model/margin_function/linear_margin_function.py
@@ -1,5 +1,7 @@
 from typing import Dict, List
 
+import numpy as np
+
 from extreme_estimator.extreme_models.margin_model.margin_function.parametric_margin_function import \
     ParametricMarginFunction
 from extreme_estimator.extreme_models.margin_model.param_function.abstract_coef import AbstractCoef
@@ -27,7 +29,10 @@ class LinearMarginFunction(ParametricMarginFunction):
     COEF_CLASS = LinearCoef
 
     def __init__(self, coordinates: AbstractCoordinates, gev_param_name_to_dims: Dict[str, List[int]],
-                 gev_param_name_to_coef: Dict[str, AbstractCoef]):
+                 gev_param_name_to_coef: Dict[str, AbstractCoef],
+                 starting_point=None):
+        # Starting point for the trend is the same for all the parameters
+        self.starting_point = starting_point
         self.gev_param_name_to_coef = None  # type: Dict[str, LinearCoef]
         super().__init__(coordinates, gev_param_name_to_dims, gev_param_name_to_coef)
 
@@ -35,6 +40,16 @@ class LinearMarginFunction(ParametricMarginFunction):
         return LinearParamFunction(dims=self.gev_param_name_to_dims[gev_param_name],
                                    coordinates=self.coordinates.coordinates_values(),
                                    linear_coef=self.gev_param_name_to_coef[gev_param_name])
+
+    def get_gev_params(self, coordinate: np.ndarray) -> GevParams:
+        if self.starting_point is not None:
+            # Shift temporal coordinate to enable to model temporal trend with starting point
+            assert self.coordinates.has_temporal_coordinates
+            idx_coordinate_t = self.coordinates.coordinates_names.index(self.coordinates.COORDINATE_T)
+            assert 0 <= idx_coordinate_t < len(coordinate)
+            if coordinate[idx_coordinate_t] < self.starting_point:
+                coordinate[idx_coordinate_t] = self.starting_point
+        return super().get_gev_params(coordinate)
 
     @classmethod
     def idx_to_coefficient_name(cls, coordinates: AbstractCoordinates) -> Dict[int, str]:

extreme_estimator/extreme_models/margin_model/parametric_margin_model.py
@@ -15,7 +15,11 @@ from spatio_temporal_dataset.coordinates.abstract_coordinates import AbstractCoo
 class ParametricMarginModel(AbstractMarginModel, ABC):
 
     def __init__(self, coordinates: AbstractCoordinates, use_start_value=False, params_start_fit=None,
-                 params_sample=None):
+                 params_sample=None, starting_point=None):
+        """
+        :param starting_point: starting coordinate for the temporal trend
+        """
+        self.starting_point = starting_point  # type: int
         self.margin_function_sample = None  # type: ParametricMarginFunction
         self.margin_function_start_fit = None  # type: ParametricMarginFunction
         super().__init__(coordinates, use_start_value, params_start_fit, params_sample)
@@ -23,6 +27,15 @@ class ParametricMarginModel(AbstractMarginModel, ABC):
     def fitmargin_from_maxima_gev(self, data: np.ndarray, df_coordinates_spat: pd.DataFrame,
                                   df_coordinates_temp: pd.DataFrame) -> ResultFromFit:
         assert data.shape[1] == len(df_coordinates_spat)
+        # assert data.shape[0] == len(df_coordinates_temp)
+
+        # Enforce a starting point for the temporal trend
+        if self.starting_point is not None:
+            ind_to_modify = df_coordinates_temp.iloc[:, 0] <= self.starting_point  # type: pd.Series
+            # Assert that some coordinates are selected but not all (at least 20 data should be left for temporal trend)
+            assert 0 < sum(ind_to_modify) < len(ind_to_modify) - 20
+            # Modify the temporal coordinates to enforce the stationarity
+            df_coordinates_temp.loc[ind_to_modify] = self.starting_point
 
         fit_params = get_margin_formula(self.margin_function_start_fit.form_dict)
 

extreme_estimator/extreme_models/utils.py
@@ -83,7 +83,9 @@ def get_coord(df_coordinates: pd.DataFrame):
 
 
 def get_margin_formula(fit_marge_form_dict) -> Dict:
-    return {k: robjects.Formula(v) for k, v in fit_marge_form_dict.items()}
+    as_null = r['as.null']
+    margin_formula = {k: robjects.Formula(v) if v != 'NULL' else as_null(1.0) for k, v in fit_marge_form_dict.items()}
+    return margin_formula
 
 # def conversion_to_FloatVector(data):
 #     """Convert DataFrame or numpy array into FloatVector for r"""
@@ -91,3 +93,4 @@ def get_margin_formula(fit_marge_form_dict) -> Dict:
 #         data = data.values
 #     assert isinstance(data, np.ndarray)
 #     return npr.numpy2ri(data)
+