04e68b3bbd3f0682ebcdbd7d5e787125c2bb5baf
Le Roux Erwan <erwan.le-roux@irstea.fr>
2019-10-25 16:35:24 +0200

    [EUROCODE DATA] add mapping from massif name to eurocode region

--- experiment/eurocode_data/massif_name_to_departement.py
+++ experiment/eurocode_data/massif_name_to_departement.py
@@ -2,6 +2,7 @@ from typing import Dict, List
 
 from experiment.eurocode_data.departement_alpes_francaises import HauteSavoie, Savoie, Isere, Drome, HautesAlpes, \
     AlpesDeHauteProvence, AlpesMaritimes, AbstractDepartementAlpesFrancaises
+from experiment.eurocode_data.eurocode_region import C1, C2, E
 
 massif_name_to_departement_types = {
     'Chablais': [HauteSavoie],
@@ -26,7 +27,34 @@ massif_name_to_departement_types = {
     'Parpaillon': [HautesAlpes, AlpesDeHauteProvence],
     'Ubaye': [AlpesDeHauteProvence],
     'Haut_Var-Haut_Verdon': [AlpesDeHauteProvence],
-    'Mercantour': [AlpesMaritimes, AlpesDeHauteProvence]}
+    'Mercantour': [AlpesMaritimes, AlpesDeHauteProvence]
+}
+
+massif_name_to_eurocode_region = {
+    'Chablais': E,
+    'Aravis': E,
+    'Mont-Blanc': E,
+    'Bauges': E,
+    'Beaufortain': E,
+    'Haute-Tarentaise': E,
+    'Chartreuse': C2, # Mainly in Isère (and small part in Savoie but belong to C2 cantons)
+    'Belledonne': C2, # Mainly in Isère (and small part in Savoie but belong to C2 cantons)
+    'Maurienne': E,
+    'Vanoise': E,
+    'Haute-Maurienne': E,
+    'Grandes-Rousses': E, # Saint-Sorlin-d'Arves belong to the Grandes ROusses, and belong to the canton of St Jean de Maurienne in Savoie which is considered E
+    'Thabor': E,
+    'Vercors': C2,
+    'Oisans': C2, # we consider they are mainly in Isere, thus C2
+    'Pelvoux': C2,
+    'Queyras': C1,
+    'Devoluy': C1, # Look on Google Map, but when we look at the mountain ranges devoluy is clearly only in Hautes Alpes
+    'Champsaur': C1,
+    'Parpaillon': C1,
+    'Ubaye': C1,
+    'Haut_Var-Haut_Verdon': C1,
+    'Mercantour': C1
+}
 
 massif_name_to_departement_objects = {m: [d() for d in deps] for m, deps in
                                       massif_name_to_departement_types.items()}  # type: Dict[str, List[AbstractDepartementAlpesFrancaises]]
