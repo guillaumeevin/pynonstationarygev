[projection snowfall] fix one special case for the formula. fix issue in model check in one fold fit. fix the parameter for one model. big bug fix in evgam.

extreme_fit/model/result_from_model_fit/result_from_extremes/result_from_evgam.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from rpy2 import robjects
 from rpy2.robjects.pandas2ri import ri2py_dataframe
 from scipy.interpolate import make_interp_spline
@@ -77,7 +78,6 @@ class ResultFromEvgam(AbstractResultFromExtremes):
     @property
     def margin_coef_ordered_dict(self):
         coefficients = np.array(self.name_to_value['coefficients'])
-        print(self.name_of_the_climatic_effects)
         param_name_to_str_formula = {k: str(v) for k, v in
                                      self.get_python_dictionary(self.name_to_value['formula']).items()}
         r_param_names_with_spline = [k for k, v in param_name_to_str_formula.items() if "s(" in v]
@@ -105,6 +105,9 @@ class ResultFromEvgam(AbstractResultFromExtremes):
                     new_dims.append(new_dim)
                 param_name_to_dims[param_name] = new_dims
             # Extract the coef list
+            coefficients = [self.load_coefficients(r_param_name) for r_param_name in self.r_param_names]
+            coefficients = np.concatenate(coefficients)
+            assert len(coefficients) == len(np.array(self.name_to_value['coefficients']))
             coef_dict = get_margin_coef_ordered_dict(param_name_to_dims, coefficients, self.type_for_mle,
                                                      dim_to_coordinate_name=self.dim_to_coordinate,
                                                      name_of_the_climatic_effects=self.name_of_the_climatic_effects)
@@ -118,26 +121,47 @@ class ResultFromEvgam(AbstractResultFromExtremes):
         else:
             dim, max_degree = dims[0]
             knots = self.load_knots(r_param_name)
-            x = np.array(self.name_to_value["data"])[1]
+            # Load the coordinates
+            data = np.array(self.name_to_value["data"])
+            x = data[1]
             y = np.array(self.get_python_dictionary(self.name_to_value[r_param_name])['fitted'])
+            if len(data) > 2:
+                x_climatic = data[2:]
+                y = self.remove_effects_from_y_from_all_climate_model(x_climatic, y, r_param_name)
             assert len(knots) == 5
-            x_for_interpolation = [int(knots[1]+1), int((knots[1] + knots[3])/2), int(knots[3]-1)]
+            a, b = 0.5, 0.5
+            x_for_interpolation = [a * knots[1] + b * knots[2], (knots[1] + knots[3])/2, b * knots[2] + a * knots[3]]
 
             # For the time covariate, the distance will be zero for the closer year
             # For the temperature covariate, the distance will be minimal for the closer covariate
             index = []
             for x_to_find in x_for_interpolation:
-                distances = np.power(x - x_to_find, 2)
+                distances = np.abs(x - x_to_find)
                 closer_index = np.argmin(distances)
                 index.append(closer_index)
 
-            x = [x[i] for i in index]
-            y = [y[i] for i in index]
+            x = np.array([x[i] for i in index])
+            y = np.array([y[i] for i in index])
             spline = make_interp_spline(x, y, k=1, t=knots)
             coefficients = spline.c
             assert len(knots) == len(coefficients) + 1 + max_degree
             dim_knots_and_coefficient[dim] = (knots, coefficients)
         return dim_knots_and_coefficient
+
+    def remove_effects_from_y_from_all_climate_model(self, x_climatic, y, r_param_name):
+        y = y.copy()
+        assert self.name_of_the_climatic_effects is not None
+        # Load the coefficient correspond to the effect from the last climate model
+        coefficients = self.load_coefficients(r_param_name)
+        effects_coefficients = coefficients[-len(self.name_of_the_climatic_effects):]
+        assert len(effects_coefficients) == len(self.name_of_the_climatic_effects) == len(x_climatic)
+        df_coordinates = pd.DataFrame(x_climatic.transpose(), columns=self.name_of_the_climatic_effects)
+        for j, effect_coef in enumerate(effects_coefficients):
+            ind = df_coordinates.iloc[:, j] == 1.0
+            assert len(ind) == len(y)
+            y[ind.values] -= effect_coef
+        return y
+
 
     def load_knots(self, r_param_name):
         try:
@@ -148,9 +172,27 @@ class ResultFromEvgam(AbstractResultFromExtremes):
             knots = []
         return knots
 
+    def load_coefficients(self, r_param_name):
+        try:
+            d = self.get_python_dictionary(self.name_to_value[r_param_name])
+            coefficients = np.array(d['coefficients'])
+            if len(self.load_knots(r_param_name)) > 0:
+                # the coefficients are not in the expected order, so we reorder them.
+                coefficients = list(coefficients)
+                new_coefficients = coefficients[:1] + coefficients[-2:] + coefficients[1:-2]
+                assert len(new_coefficients) == len(coefficients)
+                coefficients = np.array(new_coefficients)
+        except (IndexError, KeyError):
+            coefficients = []
+        return coefficients
+
     @property
     def nb_knots(self):
         return sum([len(self.load_knots(r_param_name)) for r_param_name in self.r_param_name_to_param_name.keys()])
+
+    @property
+    def r_param_names(self):
+        return ['location', 'logscale', 'shape']
 
     @property
     def r_param_name_to_param_name(self):

extreme_trend/one_fold_fit/one_fold_fit.py
@@ -181,8 +181,12 @@ class OneFoldFit(object):
             well_defined_estimators = []
             for e in estimators:
                 coordinate_values_for_the_fit = e.coordinates_for_nllh(Split.all)
-                coordinate_values_for_the_result = [np.array([self.altitude_group.reference_altitude, c])
-                                                    for c in self._covariate_before_and_after]
+
+                if isinstance(self.altitude_group, DefaultAltitudeGroup):
+                    coordinate_values_for_the_result = []
+                else:
+                    coordinate_values_for_the_result = [np.array([self.altitude_group.reference_altitude, c])
+                                                        for c in self._covariate_before_and_after]
                 coordinate_values_to_check = list(coordinate_values_for_the_fit) + coordinate_values_for_the_result
                 has_undefined_parameters = False
                 for coordinate in coordinate_values_to_check:
@@ -192,6 +196,8 @@ class OneFoldFit(object):
                         break
                 if not has_undefined_parameters:
                     well_defined_estimators.append(e)
+                else:
+                    print('undefined', e)
             estimators = well_defined_estimators
 
             if len(estimators) == 0:
@@ -291,19 +297,22 @@ class OneFoldFit(object):
 
     @property
     def stationary_estimator(self):
-        if isinstance(self.best_estimator.margin_model, AbstractGumbelAltitudinalModel):
-            return self.model_class_to_estimator_with_finite_aic[StationaryGumbelAltitudinal]
-        elif isinstance(self.best_estimator.margin_model, AltitudinalOnlyScale):
-            return self.model_class_to_estimator_with_finite_aic[StationaryAltitudinalOnlyScale]
-        elif isinstance(self.best_estimator.margin_model, AltitudinalShapeLinearTimeStationary):
-            return self.model_class_to_estimator_with_finite_aic[AltitudinalShapeLinearTimeStationary]
-        elif isinstance(self.best_estimator.margin_model, AltitudinalShapeLinearTimeStationary):
-            return self.model_class_to_estimator_with_finite_aic[AltitudinalShapeLinearTimeStationary]
-        else:
-            if isinstance(self.altitude_group, DefaultAltitudeGroup):
-                return self.model_class_to_estimator_with_finite_aic[StationaryTemporalModel]
+        try:
+            if isinstance(self.best_estimator.margin_model, AbstractGumbelAltitudinalModel):
+                return self.model_class_to_estimator_with_finite_aic[StationaryGumbelAltitudinal]
+            elif isinstance(self.best_estimator.margin_model, AltitudinalOnlyScale):
+                return self.model_class_to_estimator_with_finite_aic[StationaryAltitudinalOnlyScale]
+            elif isinstance(self.best_estimator.margin_model, AltitudinalShapeLinearTimeStationary):
+                return self.model_class_to_estimator_with_finite_aic[AltitudinalShapeLinearTimeStationary]
+            elif isinstance(self.best_estimator.margin_model, AltitudinalShapeLinearTimeStationary):
+                return self.model_class_to_estimator_with_finite_aic[AltitudinalShapeLinearTimeStationary]
             else:
-                return self.model_class_to_estimator_with_finite_aic[StationaryAltitudinal]
+                if isinstance(self.altitude_group, DefaultAltitudeGroup):
+                    return self.model_class_to_estimator_with_finite_aic[StationaryTemporalModel]
+                else:
+                    return self.model_class_to_estimator_with_finite_aic[StationaryAltitudinal]
+        except KeyError:
+            raise KeyError('A stationary model must be added either in the list of models, or here in the method')
 
     @property
     def likelihood_ratio(self):

projects/projected_extreme_snowfall/results/plot_relative_change_in_return_level.py
@@ -13,7 +13,6 @@ from spatio_temporal_dataset.coordinates.temporal_coordinates.temperature_covari
 def plot_relative_dynamic_in_return_level(massif_names, visualizer_list: List[
     AltitudesStudiesVisualizerForNonStationaryModels]):
     ax = plt.gca()
-    print(len(visualizer_list))
     visualizer = visualizer_list[0]
     assert len(massif_names) == 1
     assert visualizer.temporal_covariate_for_fit is AnomalyTemperatureWithSplineTemporalCovariate
@@ -32,7 +31,7 @@ def plot_relative_dynamic_in_return_level(massif_names, visualizer_list: List[
 
 
 def plot_curve(ax, massif_name, visualizer: AltitudesStudiesVisualizerForNonStationaryModels):
-    temperatures_list = np.linspace(1, 5, num=400)
+    temperatures_list = np.linspace(1, 4.5, num=400)
     one_fold_fit = visualizer.massif_name_to_one_fold_fit[massif_name]
     print(get_display_name_from_object_type(type(one_fold_fit.best_margin_model)),
           "significant={}".format(one_fold_fit.is_significant))