9a2ada32e5deafd7f5fa4cac58edea6013c1b2a8
Le Roux Erwan <erwan.le-roux@irstea.fr>
2021-05-03 10:22:39 +0200

    [projected snowfall] add new global mean temperature for the data

--- spatio_temporal_dataset/coordinates/temporal_coordinates/temperature_covariate.py
+++ spatio_temporal_dataset/coordinates/temporal_coordinates/temperature_covariate.py
@@ -1,3 +1,5 @@
+from extreme_data.cru_data.global_mean_temperature_until_2020 import \
+    winter_year_to_averaged_global_mean_temp_wrt_1850_1900
 from extreme_data.meteo_france_data.adamont_data.adamont_scenario import str_to_scenario, get_gcm_list, AdamontScenario
 from extreme_data.meteo_france_data.adamont_data.cmip5.climate_explorer_cimp5 import year_to_global_mean_temp, \
     year_to_averaged_global_mean_temp
@@ -58,8 +60,7 @@ class AnomalyTemperatureWithSplineTemporalCovariate(AbstractTemporalCovariateFor
         is_observations = isinstance(gcm, float) and isinstance(scenario_str, float)
         if (gcm, scenario) not in cls.gcm_and_scenario_to_d:
             if is_observations:
-                print('Loading average rcp 8.5  temeprature for the obs covariate')
-                d = year_to_averaged_global_mean_temp(AdamontScenario.rcp85, anomaly=True, spline=True)
+                d = winter_year_to_averaged_global_mean_temp_wrt_1850_1900(spline=True)
             else:
                 d = year_to_global_mean_temp(gcm, scenario, anomaly=True, spline=True)
             cls.gcm_and_scenario_to_d[(gcm, scenario)] = d
